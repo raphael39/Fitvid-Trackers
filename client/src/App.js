f849e27d7270b91f4bb157fb9bb22d0da2d4cab2
Pier Andrea <pierandreadelise@gmail.com>
2020-05-01 18:48:10 +0200

    add: pages, starting writing Router and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import Login from './pages/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1> Hello</h1>
-      
-    </div>
+    <Router>
+      <div>
+        <Route path="/login"
+          component={Login} />
+      </div>            
+    </Router>
   );
 }
 
 export default App;
+
