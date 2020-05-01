99d1c1c8e3ad113b72d9781b65ff715f0a6ede93
Pier Andrea <pierandreadelise@gmail.com>
2020-05-01 23:36:16 +0200

    add: basic structure of importvideo page/component

--- client/src/pages/ImportVideo/ImportVideo.js
+++ client/src/pages/ImportVideo/ImportVideo.js
@@ -4,7 +4,10 @@ function ImportVideo () {
 
   return (
     <div className = 'div-login'>
-      <h1>ImportVideo</h1>
+      <h1>Import Video</h1>
+      <p>Copy here the youtube link you want to import:</p>
+      <input type='text' placeholder='video url'></input>
+      <button>Create</button>
     </div>
   ) 
 }
