192442e16ddfe8bdd13ab8aa5989571f7eb5b298
Pier Andrea <pierandreadelise@gmail.com>
2020-05-01 20:28:22 +0200

    add: login.css, className in Login

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from '../../Asset/Screenshot from 2020-05-01 18-39-32.png'
+import './login.css'
 
 function Login () {
 
   return (
-    <div>
-      <img src={logo}></img>
+    <div className = 'div-login'>
+      <img src={logo} className='logo'></img>
       <h1>Login</h1>
-      <p>WELCOME TO FITVID TRACKER, THE FIRST WEBSITE/APP TO CREATE YOUR PERSONAL WORKOUT USING YOUTUBE VIDEOS</p>
+      <p className='description'>WELCOME TO FITVID TRACKER, THE FIRST WEBSITE/APP TO CREATE YOUR PERSONAL WORKOUT USING YOUTUBE VIDEOS</p>
       <button>Login with Google</button>
       <button>Login with Youtube</button>
     </div>
