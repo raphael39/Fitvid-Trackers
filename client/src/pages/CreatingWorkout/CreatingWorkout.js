f12f281e386189fdde74f238adc93b701ae60518
Pier Andrea <pierandreadelise@gmail.com>
2020-05-01 23:55:32 +0200

    add: creatingworkout mocking data

--- client/src/pages/CreatingWorkout/CreatingWorkout.js
+++ client/src/pages/CreatingWorkout/CreatingWorkout.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+//mock data 
+
+//youtube url, please note that the user copied an url with time too, to consider as case scenario
+const url = 'https://www.youtube.com/watch?v=vc1E5CfRfos&t=563s'
+//day selected for the workout
+const day = 'Wednesday'
+
 function CreatingWorkout () {
 
 
