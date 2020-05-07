5c438d33feb0b4e632693a1e9347223d5771fcc0
Petr Penicka <petr.penicka@gmail.com>
2020-05-08 00:30:17 +0200

    feat: first working HomePage prototype

--- client/src/App.js
+++ client/src/App.js
@@ -11,21 +11,20 @@ import Login from './pages/Login/Login';
 import ImportVideo from './pages/ImportVideo/ImportVideo';
 import Workout from './pages/Workout/Workout';
 import SetCredentials from './pages/SetCredentials/SetCredentials';
-import CreateWorkout from './pages/CreateWorkout/CreateWorkout'
+import CreateWorkout from './pages/CreateWorkout/CreateWorkout';
 import ListOfWorkouts from './pages/ListOfWorkouts/ListOfWorkouts';
 import myListOfWorkouts from './pages/ListOfWorkouts/myListOfWorkouts';
 import HomePage from './pages/HomePage/HomePage';
+import persistedReducer from './reducers';
 import ListOfWorkoutPlans from './pages/ListOfWorkoutPlans/ListOfWorkoutPlans';
 import myListOfWorkoutPlans from './pages/ListOfWorkoutPlans/MyListOfWorkoutPlans'
 import WorkoutPlan from './pages/WorkoutPlan/WorkoutPlan';
 import CreateWorkoutPlan from './pages/CreateWorkoutPlan/CreateWorkoutPlan'
 
-
-import rootReducer from './reducers';
 import './App.css';
 import MyListOfWorkouts from './pages/ListOfWorkouts/myListOfWorkouts';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)));
 const persistor = persistStore(store);
 
 function App () {
@@ -49,16 +48,16 @@ function App () {
               component={Workout} />
             <Route exact path="/workoutList"
               component={ListOfWorkouts} />
-              <Route exact path="/myListOfWorkouts"
-          component={MyListOfWorkouts} />
-          <Route exact path="/ListOfWorkoutPlans"
-          component={ListOfWorkoutPlans} />
-          <Route exact path="/myListOfWorkoutPlans"
-          component={myListOfWorkoutPlans} />
-          <Route exact path="/WorkoutPlan"
-          component={WorkoutPlan} />
-          <Route exact path="/CreateWorkoutPlan"
-          component={CreateWorkoutPlan} />
+            <Route exact path="/myListOfWorkouts"
+              component={MyListOfWorkouts} />
+            <Route exact path="/ListOfWorkoutPlans"
+              component={ListOfWorkoutPlans} />
+            <Route exact path="/myListOfWorkoutPlans"
+              component={myListOfWorkoutPlans} />
+            <Route exact path="/WorkoutPlan"
+              component={WorkoutPlan} />
+            <Route exact path="/CreateWorkoutPlan"
+              component={CreateWorkoutPlan} />
           </Router>
         </PersistGate>
       </Provider>
--- server/src/models/schedule.js
+++ server/src/models/schedule.js
@@ -5,7 +5,7 @@ const { WorkoutSchema } = require('./workout');
 const ScheduleSchema = mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' },
   map: [{
-    day: Number,
+    day: Date,
     workouts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Workout' }]
   }]
 });
