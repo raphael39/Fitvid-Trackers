b4f035a9276920f89302c50f63fc7c2b2817852a
Slug-Man <Slug-Man@users.noreply.github.com>
2020-05-08 09:56:40 +0100

    feat: update a workout

--- server/src/routes/workout.js
+++ server/src/routes/workout.js
@@ -1,11 +1,12 @@
 const Router = require('@koa/router');
 
-const { getWorkout, createWorkout } = require('../controllers/workout');
+const { getWorkout, createWorkout, updateWorkout } = require('../controllers/workout');
 const auth = require('../middlewares/auth-middleware');
 const router = new Router({ prefix: '/workout' });
 
 router
   .get('/:id', auth, getWorkout)
+  .post('/:id', auth, updateWorkout)
   .post('/', auth, createWorkout);
 
 module.exports = router;
