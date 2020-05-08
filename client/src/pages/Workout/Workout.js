c13f2dc1795efacf527e3f58b6ff8a23a162b67a
Pier Andrea <pierandreadelise@gmail.com>
2020-05-09 00:22:34 +0200

    fix: states public -> isPublic workout, difficulty->difficulties workout, countdown and stopwatch visible only in editable false  in workout

--- client/src/pages/Workout/Workout.js
+++ client/src/pages/Workout/Workout.js
@@ -15,7 +15,7 @@ import Stopwatch from '../../components/Stopwatch/Stopwatch';
 
 
 //mock data
-const workout = {id:"randomNumber", publicWorkout:true, workoutName:"AthleanX, fullbody", youtubeId:"vc1E5CfRfos", days: {monday: true, tuesday: false, wednesday: true, thursday: false, friday: false, saturday: false, sunday: false}, description: "Full body, bodyweight exercises with two different plans",difficulties: {easy: false, medium: true, hard: false}, exercises: [{ name: "Pull-ups", sets: 3, reps: 20, timestamp: "15"}, { name: "Abs ", sets: 2, reps: 1, timestamp: "3:00"}]}
+const workout = {id:"randomNumber", isPublic:true, name:"AthleanX, fullbody", youtubeId:"vc1E5CfRfos", days: {monday: true, tuesday: false, wednesday: true, thursday: false, friday: false, saturday: false, sunday: false}, description: "Full body, bodyweight exercises with two different plans",difficulties: {easy: false, medium: true, hard: false}, exercises: [{ name: "Pull-ups", sets: 3, reps: 20, timestamp: "15"}, { name: "Abs ", sets: 2, reps: 1, timestamp: "3:00"}]}
 
 function Workout ({
   //receive the workout:id obj
@@ -27,7 +27,7 @@ function Workout ({
   const [description, setDescription] = useState('');
   const [difficulties, setDifficulties] = useState({easy:false, medium:false, hard:false});
   const [days, setDays] = useState({monday:false, tuesday:false, wednesday:false, thursday:false, friday:false, saturday:false, sunday:false});
-  const [publicWorkout, setPublicWorkout] = useState(false);
+  const [isPublic, setIsPublic] = useState(false);
 
   //videoplayer states, work on table if status editable=false
   const [timeVideo, setTimeVideo] = useState();
@@ -40,7 +40,7 @@ function Workout ({
     setDifficulties(workout.difficulties);
     setDays(workout.days);
     setWorkoutName(workout.workoutName);
-    setPublicWorkout(workout.publicWorkout)
+    setIsPublic(workout.isPublic)
   }, [])
 
   return (
@@ -52,13 +52,16 @@ function Workout ({
       <div className='div-Workout'>
         <NameWorkout workoutName={workoutName} setWorkoutName={setWorkoutName} editable={editable}/>
         <YoutubePlayer url={`https://www.youtube.com/watch?v=${workout.youtubeId}`} timeVideo={timeVideo} clickTimestamp={clickTimestamp} />
-        <Countdown/>
-        <Stopwatch/>
+        {!editable && 
+          <div>
+            <Countdown/>
+            <Stopwatch/>
+          </div>}
         {exercises && <Table exercises={exercises} setExercise={setExercise} editable={editable} setTimeVideo={setTimeVideo} setClickTimestamp={setClickTimestamp} clickTimestamp={clickTimestamp} />}
         <DescriptionWorkout description={description} setDescription={setDescription} editable={editable}/>
         <DifficultyWorkout difficulties={difficulties} setDifficulties={setDifficulties} editable={editable}/>
         <DaysWorkout days={days} setDays={setDays} editable={editable}/>
-        <PublicWorkout publicWorkout={publicWorkout} setPublicWorkout={setPublicWorkout} editable={editable}/>
+        <PublicWorkout isPublic={isPublic} setIsPublic={setIsPublic} editable={editable}/>
       </div>
     </div>
   )
