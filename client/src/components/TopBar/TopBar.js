287147d94536233bf33855591834d9ee10bb6687
Petr Penicka <petr.penicka@gmail.com>
2020-05-11 10:40:29 +0200

    fix: correct reset of state on user logout

--- client/src/components/TopBar/TopBar.js
+++ client/src/components/TopBar/TopBar.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from 'react-router-dom';
 import { setUser } from '../../redux/actions/userActions';
 import { setSchedule } from '../../redux/actions/scheduleActions';
 
 function TopBar () {
   const dispatch = useDispatch();
   const user = useSelector(state => state.currentUser);
+  const history = useHistory();
 
   const logOut = () => {
     dispatch(setUser(null));
     dispatch(setSchedule(null));
-    window.location.href="/";
+    history.push('/');
   }
 
   return (
