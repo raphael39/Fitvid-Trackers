b129b3dfb826fbf3d0f158b5d5ccdf7fd788a2d5
Pier Andrea <pierandreadelise@gmail.com>
2020-05-13 18:32:58 +0200

    style: navbar and logout button

--- client/src/components/Navigation/navBar.js
+++ client/src/components/Navigation/navBar.js
@@ -11,9 +11,11 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import {Link} from 'react-router-dom'
-import {Button} from '@material-ui/core'
+import { Link } from 'react-router-dom'
+import { Button } from '@material-ui/core'
 import TopBar from '../../components/TopBar/TopBar';
+import Grid from '@material-ui/core/Grid';
+
 
 // redux
 
@@ -21,51 +23,74 @@ import { useSelector } from "react-redux";
 
 
 export default function NavBar() {
-    const classes = useStyles();
-    const [auth, setAuth] = React.useState(true);
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
   //redux
   const user = useSelector(state => state.currentUser);
-  
-    const handleChange = (event) => {
-      setAuth(event.target.checked);
-    };
-  
-    const handleMenu = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-  
-    return (
 
-        
-        <AppBar className={classes.root} position="static">
-          <Toolbar>
-            <Link  className={classes.title} to='/HomePage'>
-            <Typography variant="h6" className={classes.title}>
-              Home
-            </Typography>
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+
+
+    <AppBar className={classes.root} position="sticky">
+      <Toolbar>
+        <Grid container position="row" alignItems="center" justify="space-evenly" spacing={3}>
+
+          <Grid item align="center" xs={2} >
+            <Link className={classes.title} to='/HomePage'>
+              <Typography variant="h6" className={classes.title}>
+                Home
+              </Typography>
             </Link>
-            <Link  className={classes.title} to='/createWorkout'>
-            <Typography variant="h6" className={classes.title}>
-              Create Workout
-            </Typography>
+          </Grid>
+
+          <Grid item align="center" xs={2}>
+            <Link className={classes.title} to='/createWorkout'>
+              <Typography variant="h6" className={classes.title}>
+                Create Workout
+              </Typography>
             </Link>
-            <Link  className={classes.title} to='/listOfWorkouts'>
-            <Typography variant="h6" className={classes.title}>
-              Browse Workouts
-            </Typography>
+          </Grid>
+
+          <Grid item align="center" xs={2}>
+            <Link className={classes.title} to='/listOfWorkouts'>
+              <Typography variant="h6" className={classes.title}>
+                Browse Workouts
+              </Typography>
             </Link>
-            <Link  className={classes.title} to='/listOfworkoutplans'>
-            <Typography variant="h6" className={classes.title}>
-              WorkoutPlans
-            </Typography>
+          </Grid>
+
+          <Grid item align="center" xs={2}>
+            <Link className={classes.title} to='/CreateWorkoutPlan'>
+              <Typography variant="h6" className={classes.title}>
+                Create WorkoutPlans
+                </Typography>
             </Link>
+          </Grid>
+
+          <Grid item align="center" xs={2}>
+            <Link className={classes.title} to='/listOfworkoutplans'>
+              <Typography variant="h6" className={classes.title}>
+                Browse WorkoutPlans
+                </Typography>
+            </Link>
+          </Grid>
+
+          <Grid item align="center" xs={2} >
             {user && (
               <div>
                 <IconButton
@@ -96,27 +121,33 @@ export default function NavBar() {
                 </Menu>
               </div>
             )}
-          </Toolbar>
-        </AppBar>
-
-    );
-  }
+          </Grid>
 
 
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-      backgroundColor: 'black',
-      paddingLeft: "5%",
-      paddingRight: "5%"
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-      color: 'white',
-      textDecoration: 'none',
-    },
-    
-  }));
+
+
+
+        </Grid>
+      </Toolbar>
+    </AppBar>
+
+  );
+}
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: 'black',
+
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    color: 'white',
+    textDecoration: 'none',
+  },
+
+}));
--- client/src/components/TopBar/TopBar.js
+++ client/src/components/TopBar/TopBar.js
@@ -3,8 +3,21 @@ import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from 'react-router-dom';
 import { setUser } from '../../redux/actions/userActions';
 import { setSchedule } from '../../redux/actions/scheduleActions';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    '&:hover': {
+      backgroundColor: 'black',
+      color: "white"
+    }
+  }
+}));
 
 function TopBar () {
+
+  const classes = useStyles()
   const dispatch = useDispatch();
   const user = useSelector(state => state.currentUser);
   const history = useHistory();
@@ -18,7 +31,7 @@ function TopBar () {
   return (
     <div className="TopBar">
       {(user) ? <span>{user.firstName} {user.lastName} ({user.email}) </span> : null}
-      <button onClick={logOut}>Log out</button>
+      <Button className={classes.button} onClick={logOut}>Log out</Button>
     </div>
   )
 }
