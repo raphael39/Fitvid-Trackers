703a17fe7b6689e14d81cdbe06b378f730e27435
Slug-Man <Slug-Man@users.noreply.github.com>
2020-05-13 14:08:09 +0100

    add plan delete route, plan perms handling and better http response codes

--- server/src/models/plan.js
+++ server/src/models/plan.js
@@ -4,7 +4,9 @@ const { WorkoutSchema } = require('./workout');
 
 const PlanSchema = new mongoose.Schema({
   name: String,
-  workoutList: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Workout' }]
+  workoutList: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Workout' }],
+  createdBy: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile'},
+  isPublic: Boolean
 });
 
 const Plan = mongoose.model('Plan', PlanSchema);
