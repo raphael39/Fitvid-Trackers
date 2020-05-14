a0c22d92061ad0219558c4ee1057f2f1cb93af9d
Slug-Man <Slug-Man@users.noreply.github.com>
2020-05-14 11:18:08 +0100

    fix: on workout deletion remove references from schedule and plan

--- server/src/models/workout.js
+++ server/src/models/workout.js
@@ -20,6 +20,22 @@ const WorkoutSchema = mongoose.Schema({
   isPublic: Boolean
 });
 
+WorkoutSchema.pre('remove', async function () {
+  await mongoose.model('Plan').update(
+    { workoutList: this._id },
+    { $set: { "workoutList.$[objid]": null } },
+    { 
+      multi: true,
+      arrayFilters: [ { objid: this._id } ]
+    }
+  );
+  await mongoose.model('Schedule').update(
+    { },
+    { $pull: { map: { workout: this } } },
+    { multi: true }
+  );
+});
+
 const Workout = mongoose.model('Workout', WorkoutSchema);
 
 module.exports = {
